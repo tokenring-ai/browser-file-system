785e90d7bf9dcc343578309869af3dd60dd8a2d2
Mark Dierolf <mdierolf@cloudquote.io>
2025-07-19 13:21:33 -0700

    Initial release of web-terminal with git submodules

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-export { default as BrowserFileSystemService } from './BrowserFileSystem.js';
+export { default as BrowserFileSystemService } from "./BrowserFileSystem.js";
 
 export const name = "@token-ring/browser-file-system";
-export const description = "Provides a browser-based mock FileSystemService using in-memory data.";
+export const description =
+	"Provides a browser-based mock FileSystemService using in-memory data.";
 export const version = "0.1.0";
